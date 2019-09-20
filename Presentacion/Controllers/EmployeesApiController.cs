f09ef3caee9f1c72102665216ae11c4c1a1fc2be
Kirlian17 <55565372+Kirlian17@users.noreply.github.com>
2019-09-19 21:51:20 -0500

    Add files via upload

---
+++ Presentacion/Controllers/EmployeesApiController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using SalarioEmpleados.Model;
+using SalarioEmpleados.Busines;
+
+namespace Presentacion.Controllers
+{
+    public class EmployeesApiController : ApiController
+    {
+        // GET: api/EmployeesApi
+        [HttpGet]
+        public IEnumerable<Employees> Get()
+        {
+
+
+            EmployeesRules employeesRules = new EmployeesRules();
+            List<Employees> employeesList = employeesRules.GetEmployees();
+
+                return employeesList;
+        }
+
+        // GET: api/EmployeesApi/5
+        [HttpGet]
+        public List<Employees> Get(int id)
+        {
+            EmployeesRules employeesRules = new EmployeesRules();
+            List<Employees> employeesList = employeesRules.GetEmployees();
+            List<Employees> result = new List<Employees>();
+            
+            Employees employed = employeesList.FirstOrDefault(x => x.id == id);
+            result.Add(employed);
+
+            return result;
+        }  
+    }
+}
---
+++ Presentacion/Controllers/EmployeesController.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using SalarioEmpleados.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentacion.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                List<Employees> employeesList = GetEmployeesLocalApi();
+                return View(employeesList);
+            }
+
+            else
+            {
+                List<Employees> employeesList = new List<Employees>();
+                Employees employed = GetEmployedLocalApi(id);
+                employeesList.Add(employed);
+                return View(employeesList);
+            }
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Employees(int id)
+        {
+            Employees employed = GetEmployedLocalApi(id);
+            return View(employed);
+        }
+
+        public List<Employees> GetEmployeesLocalApi()
+        {
+            List<Employees> employeesinformation = new List<Employees>();
+            string baseUrl;
+
+                baseUrl = "https://localhost:44300/api/employeesapi";
+            
+            System.Net.Http.HttpResponseMessage response = null;
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                response = client.GetAsync(baseUrl).Result;
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                var employedResponse = response.Content.ReadAsStringAsync().Result;
+                employeesinformation = JsonConvert.DeserializeObject<List<Employees>>(employedResponse);
+            }
+
+            return employeesinformation;
+        }
+
+        public Employees GetEmployedLocalApi(int? id)
+        {
+            Employees employeesinformation = new Employees();
+            string baseUrl;
+
+                baseUrl = $"https://localhost:44300/api/employeesapi/{id}";
+
+            System.Net.Http.HttpResponseMessage response = null;
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                response = client.GetAsync(baseUrl).Result;
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                var employedResponse = response.Content.ReadAsStringAsync().Result;
+                employeesinformation = JsonConvert.DeserializeObject<Employees>(employedResponse);
+            }
+
+            return employeesinformation;
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
